008db79dff4903a937f5d86ac0d77ead2bb19bbe
Helmar <TheHelmar@users.noreply.github.com>
2020-02-13 20:28:39 +0100

    Fixed beta/non-beta issues and some error handling

--- seUserCards.user.js
+++ seUserCards.user.js
@@ -2,7 +2,7 @@
 // @name         Better SE User Card overlay
 // @namespace    https://github.com/TheHelmar/seUserCards
 // @homepage     https://github.com/TheHelmar/seUserCards
-// @version      0.3
+// @version      0.4
 // @description  Provides a new User Card showing question & answer count
 // @author       Helmar
 // @match        https://*.stackexchange.com/questions*
@@ -24,7 +24,6 @@
     [].forEach.call(posts, function(posts) {
         //replacing all user flairs
         let userURL = `` + posts.querySelector("a");
-        console.log(userURL);
         //Query User Page
         let userPage = document.implementation.createHTMLDocument("");
         let userPageData;
@@ -32,8 +31,14 @@
         $.get(userURL, function(userPageData, status){
             userPage.documentElement.innerHTML = userPageData;
             //Get counts
-            let questionCount=/[0-9]+/g.exec(userPage.querySelector("#user-panel-questions > div.subheader.p0.grid.ai-center > h3 > a > span").innerHTML);
-            let answerCount=/[0-9]+/g.exec(userPage.querySelector("#user-panel-answers > div.subheader.p0.grid.ai-center > h3 > a > span").innerHTML);
+            let questionCount="Error";
+            let answerCount="Error";
+            //Full Sites
+            try{questionCount=/[0-9,]+/g.exec(userPage.querySelector("#user-card > div > div.grid--cell.fl1.wmn0 > div > div.grid--cell.fl-shrink0.pr24.wmx3 > div.fc-medium.mb16 > div > div:nth-child(2) > div > div.grid--cell.fs-body3.fc-dark.fw-bold").innerHTML);}catch(err){}
+            try{answerCount=/[0-9,]+/g.exec(userPage.querySelector("#user-card > div > div.grid--cell.fl1.wmn0 > div > div.grid--cell.fl-shrink0.pr24.wmx3 > div.fc-medium.mb16 > div > div:nth-child(1) > div > div.grid--cell.fs-body3.fc-dark.fw-bold").innerHTML);}catch(err){}
+            //Beta sites
+            try{questionCount=/[0-9]+/g.exec(userPage.querySelector("#user-panel-questions > div.subheader.p0.grid.ai-center > h3 > a > span").innerHTML);}catch(err){}
+            try{answerCount=/[0-9]+/g.exec(userPage.querySelector("#user-panel-answers > div.subheader.p0.grid.ai-center > h3 > a > span").innerHTML);}catch(err){}
             //Build new User Card
             let newUserCard = `
 <table>
@@ -47,8 +52,11 @@
 </tr>
 </table>
 `
-            //Replace User Flair
-            posts.querySelector("div").innerHTML=newUserCard;
+            //Replace User Flair if values are available
+            if(questionCount!="Error"&&answerCount!="Error")
+            {
+                posts.querySelector("div").innerHTML=newUserCard;
+            }
         });//userPage
     });//Loop over flairs
 })();
